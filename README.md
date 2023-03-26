a63b0d4d836ffde008453fe920fdaba7235c3132
alpklc <alpklc34@outlook.com>
2023-03-26 11:07:00 +0300

    second commit

---
+++ README.md
@@ -0,0 +1,5 @@
+- `flask run`
+
+## CURL
+- `curl http://127.0.0.1/5000/hello/mehmet`
+- `curl -I http://127.0.0.1/5000/hello/mehmet`
--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask import render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/dashboard")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('dashboard.html')
+
+
+@app.route('/hello')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('hello.html', name=name)
+
+
+@app.route('api/user')
+def user_api():
+    return {
+        "username": "John",
+        "surname": "Doe",
+        "age": 30
+    }
